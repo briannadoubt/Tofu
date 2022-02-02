e53cc811ecd3542616ed63a66c9dcadd7eeb69e0
Brianna Doubt <bri@briannadoubt.com>
2022-02-01 22:38:49 -0800

    Initial Commit

---
+++ Shared/ToFuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToFuApp.swift
+//  Shared
+//
+//  Created by Bri on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ToFuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
